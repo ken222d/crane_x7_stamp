ba7347f653de619f6b53f6cdaee88be67103f693
Yamamoto Haruto <hrtymmt1604@gmail.com>
2024-12-12 18:10:10 +0900

    z_hosei

--- src/press_the_stamp_tf.cpp
+++ src/press_the_stamp_tf.cpp
@@ -152,7 +152,7 @@ private:
     joint_values.push_back(angles::from_degrees(0.0));
     joint_values.push_back(angles::from_degrees(85));
     joint_values.push_back(angles::from_degrees(0.0));
-    joint_values.push_back(angles::from_degrees(-160));
+    joint_values.push_back(angles::from_degrees(-158));
     joint_values.push_back(angles::from_degrees(0.0));
     joint_values.push_back(angles::from_degrees(-50));
     joint_values.push_back(angles::from_degrees(90));
@@ -183,25 +183,18 @@ private:
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 void stamping(tf2::Vector3 target_position)
   {
     const double GRIPPER_DEFAULT = 0.0;
     const double GRIPPER_OPEN = angles::from_degrees(60.0);
     const double GRIPPER_CLOSE = angles::from_degrees(15.0);
-    const int move_steps = 3;
+    const int move_steps = 4;
     const double before_press_z = 0.05;
+
+
+    // ハンドを閉める
+    control_gripper(GRIPPER_DEFAULT);
+
 
 	// ホームポジションへ移動（重複している可能性があり, 後から削除する可能性が高い）
 	init_pose();
@@ -209,11 +202,11 @@ void stamping(tf2::Vector3 target_position)
     // 現在位置を取得
     geometry_msgs::msg::Pose current_pose = move_group_arm_->getCurrentPose().pose;
 
-    // ハンドを閉める
-    control_gripper(GRIPPER_DEFAULT);
+
+	
 
 
-    // 経路を3分割して(target_position.x(), target_position.y(), 0.5)まで移動
+    // 経路を4分割して(target_position.x(), target_position.y(), 0.3)まで移動
     for (int i = 1; i <= move_steps; ++i) {
         geometry_msgs::msg::Pose intermediate_pose;
         intermediate_pose.position.x = current_pose.position.x + (target_position.x() - current_pose.position.x) * i / move_steps;
@@ -236,11 +229,20 @@ void stamping(tf2::Vector3 target_position)
     //move_group_arm_->setPoseTarget(current_pose);  // 例: 更新した位置をターゲットとして設定
 
 
-    for (int i = 2; i >= 1; i--) {
+
+    double calculated_arm_length;
+    calculated_arm_length = std::sqrt((target_position.x() * target_position.x()) + (target_position.y() * target_position.y()));
+
+    double point_z = 0.0;
+    point_z = ((-0.12 * calculated_arm_length) + 0.07); 
+
+
+
+    for (int i = 3; i >= 1; i--) {
       geometry_msgs::msg::Pose intermediate_pose;
 	intermediate_pose.position.x = current_pose.position.x;
 	intermediate_pose.position.y = current_pose.position.y;
-        intermediate_pose.position.z = (0.1 * i);
+        intermediate_pose.position.z = ((0.1 - point_z) * i);
        // intermediate_pose.orientation = current_pose.orientation; // 同じ姿勢を維持
 
         control_arm(intermediate_pose.position.x, intermediate_pose.position.y, intermediate_pose.position.z, 90, 0, 90);
@@ -249,13 +251,18 @@ void stamping(tf2::Vector3 target_position)
       
     }
 
+
     rclcpp::sleep_for(std::chrono::seconds(2)); 
 
-    std::cout << "目標の座標に到達しました" << std::endl;
+  /*  std::cout << "目標の座標に到達しました" << std::endl;
 
 	double calculated_arm_length;
 	double theta = 0.0;
-	calculated_arm_length = std::sqrt((target_position.x() * target_position.x()) + (target_position.y() * target_position.y()));
+//	calculated_arm_length = std::sqrt((target_position.x() * target_position.x()) + (target_position.y() * target_position.y()));
+
+
+	calculated_arm_length = 0.5;
+
 	std::cout << "現在のZ座標" << current_pose.position.z  << std::endl;
 	
 	theta = angles::to_degrees(std::asin(before_press_z / calculated_arm_length));
@@ -265,7 +272,9 @@ void stamping(tf2::Vector3 target_position)
 	for (int i = 0; i < 5; ++i) {
 	move_specific_joint(1, -theta / 5); // -1°ずつ動かす
 	std::cout << "Step " << (i + 1) << ": Joint moved by" << theta / 5 << "°" << std::endl;
-	}
+	}*/
+
+    control_arm(0.3, 0, 0.3, 90, 0, 90);
 
 	// 初期姿勢に戻る
     init_pose();
